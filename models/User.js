a421f90f4566b93766afb5c1cc1fa2f8594508fc
squarepancakes <goh.shuyun@gmail.com>
2019-10-18 19:40:08 +0800

    remove select false from userSchema

--- models/User.js
+++ models/User.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
 	username: { type: String, required: true, unique: true },
-	password: { type: String, required: true, select: false },
+	password: { type: String, required: true },
 	recipes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Recipe" }]
 });
 
-
-// userSchema.virtual('recipes', {
-//     ref: 'Recipe',
-//     localField: '_id',
-//     foreignField: 'recipes'
-// });
+userSchema.pre("save", async function(next) {
+	const rounds = 7;
+	this.password = await bcrypt.hash(this.password, rounds);
+	next();
+});
 
 const User = mongoose.model("User", userSchema);
 
